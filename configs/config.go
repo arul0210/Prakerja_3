8c305f78b7a82cfff36e5276172af30610515c95
arul0210 <sahrulfirmansyah547@gmail.com>
2023-05-13 07:38:12 +0700

    mvc

--- configs/config.go
+++ configs/config.go
@@ -1,8 +1,11 @@
 package configs
 
 import (
+	"fmt"
+	"os"
 	"prakerja3/models"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,13 +13,30 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "root:110597@tcp(127.0.0.1:3306)/prakerja_batch_3?charset=utf8mb4&parseTime=True&loc=Local"
+	loadenv()
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPassword, dbHost, dbPort, dbName)
+
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("connect database failed")
 	}
 	migratDatabase()
+}
+
+func loadenv() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Gagal mengambil file env")
+	}
 }
 
 func migratDatabase() {
